4dfea13019d7f3f324fa093081a6287dba75b47f
superboy632 <86768977+superboy632@users.noreply.github.com>
2024-09-15 09:39:48 +0300

    Delete l1_2/target directory

--- l1_2/target/debug/deps/l1_2-b9c152c2c7f0fc52.d
+++
@@ -1,5 +0,0 @@
-/Users/grigri/RustroverProjects/fastRUST_wb/l1_2/target/debug/deps/l1_2-b9c152c2c7f0fc52: src/main.rs
-
-/Users/grigri/RustroverProjects/fastRUST_wb/l1_2/target/debug/deps/l1_2-b9c152c2c7f0fc52.d: src/main.rs
-
-src/main.rs:
--- l1_2/target/debug/l1_2.d
+++
@@ -1,1 +0,0 @@
-/Users/grigri/RustroverProjects/fastRUST_wb/l1_2/target/debug/l1_2: /Users/grigri/RustroverProjects/fastRUST_wb/l1_2/src/main.rs
