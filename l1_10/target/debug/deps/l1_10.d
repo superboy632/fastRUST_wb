6abc5fcdf61e7628b2b0b9f2556c2540cb85d960
superboy632 <86768977+superboy632@users.noreply.github.com>
2024-09-15 09:39:38 +0300

    Delete l1_10/target directory

--- l1_10/target/debug/deps/l1_10.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Grigorii\RustroverProjects\fastRUST_wb\l1_10\target\debug\deps\l1_10.exe: src/main.rs
-
-C:\Users\Grigorii\RustroverProjects\fastRUST_wb\l1_10\target\debug\deps\l1_10.d: src/main.rs
-
-src/main.rs:
--- l1_10/target/debug/l1_10.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Grigorii\RustroverProjects\fastRUST_wb\l1_10\target\debug\l1_10.exe: C:\Users\Grigorii\RustroverProjects\fastRUST_wb\l1_10\src\main.rs
